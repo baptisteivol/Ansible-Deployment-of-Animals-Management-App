f3713cd7df887e6307366f12f8f97c1670c8881e
Baptiste IVOL <bivol@linagora.com>
2019-12-06 17:40:50 -0500

    Update

--- Schema_BD.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `animals` (
-  `name` varchar(255) NOT NULL,
-  `favorite_color` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
